d1ff7653c9fb99911e4303ff29a858a98ca8fe30
Ilia_Isakhin <ilia_isakhin@epam.com>
2019-07-23 15:55:23 +0400

    initial

---
+++ src/main/java/com/epam/edu/spring/core/template/SpringCoreTemplate.java
@@ -0,0 +1,14 @@
+package com.epam.edu.spring.core.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreTemplate {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTemplate.class, args);
+		//TODO get service bean and do some business logic with DTO`s
+	}
+
+}
